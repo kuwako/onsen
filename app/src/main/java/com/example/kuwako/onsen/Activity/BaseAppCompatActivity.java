d1ecfa063c7544793d90cd7a57ee2daef28e7042
kuwako <kuwako@leverages.jp>
2016-01-20 20:23:08 +0900

    検索画面見た目だけ作成

---
+++ app/src/main/java/com/example/kuwako/onsen/Activity/BaseAppCompatActivity.java
@@ -0,0 +1,15 @@
+package com.example.kuwako.onsen.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kuwako on 2016/01/20.
+ */
+public class BaseAppCompatActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/kuwako/onsen/Activity/MainActivity.java
+++ app/src/main/java/com/example/kuwako/onsen/Activity/MainActivity.java
@@ -1,15 +1,17 @@
-package com.example.kuwako.onsen;
+package com.example.kuwako.onsen.Activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.kuwako.onsen.R;
+
+// 検索画面
+public class MainActivity extends BaseAppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
