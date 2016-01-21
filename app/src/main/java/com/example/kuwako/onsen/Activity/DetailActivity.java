71b1ae2cb264cd531ecb54af3828dd5d0f0b1059
kuwako <kuwako@leverages.jp>
2016-01-21 19:13:46 +0900

    必須Activity追加

---
+++ app/src/main/java/com/example/kuwako/onsen/Activity/DetailActivity.java
@@ -0,0 +1,8 @@
+package com.example.kuwako.onsen.Activity;
+
+/**
+ * Created by kuwako on 2016/01/21.
+ */
+public class DetailActivity extends BaseAppCompatActivity {
+
+}
---
+++ app/src/main/java/com/example/kuwako/onsen/Activity/MapActivity.java
@@ -0,0 +1,10 @@
+package com.example.kuwako.onsen.Activity;
+
+/**
+ * Created by kuwako on 2016/01/21.
+ */
+
+// マップ画面Activity
+public class MapActivity extends BaseAppCompatActivity {
+    
+}
